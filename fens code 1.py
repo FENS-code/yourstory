3f8ccf5ed7f454812c908d71cc869e83efd96001
FENS_code <4896eth@gmail.com>
2025-09-15 15:47:20 +0800

    Add files via upload

---
+++ fens code 1.py
@@ -0,0 +1,91 @@
+print(r'''
+                                                     8LLCf8                                         
+                                                    GLG08f8                                         
+                                                  0LC000CL                                          
+                                    8           8CL00008f0                                          
+                                   LCL         GLG00000Gf                                           
+                                   f0CL      0LC0000000LG                                           
+                                   LC8CC   8CL000000000f8                                           
+                                   GL00CL80LG000000000GL                                            
+                                   0f000Gff00000000008LG                                            
+                                    f00000CL0000000000f    80GGGCG                                  
+                                    LG000000000000000GtGCCCCGGGLfG                                  
+                                    CC000000000000000Gf0000GGLtL8                                   
+                               0LLC8GL0000000000GCCCGGG0GCLftf0                                     
+                             Gi:::;1tL80000000GLLLLLLLLLLLff0                                       
+                    8GCLftt11;;ii;;i;;ttfLCGGCLLLLLLLLLLffC                                         
+               0CLt1iiiiiiiiii1iiiii1;;iiiiii1tLLLLLLLLfL8                                          
+           8Gf1iiiiiiiiiiiiiiiiiiiiii1;i11iiiii:1tfLLLtC                                            
+         Gf1iiiiiiiiiiiiiiiiiiiiiiiiiii;iiiiii;iiiiitt0                                             
+      8C1iiiiiiiiiiiiiiiiiiiiiiiiiiiiii:iiii;i1iiiiiiifG                                            
+     C1;iiiiiiiiiii;i1ttt1i;iiiiiiiiii1;i1iii;iiiiiiiiii1C                                          
+   8t;iiiiiiiiiii;tG8     8Gt;iiiiiiiiii:iiiii;iiiiiiiiii;1C                                        
+  0i;1iiiiiiiiii;C     8     G;iiiiiiiii:;iiiii:iiiiiiiii1i;1G                            80GCftt1i1
+Gfft1iiiiiiiii1;f     8iG80008f;1iiiiii1:;;iiii;iiiiiiiiiiiii:f8                      0GLt1i;;;;iifG
+1fLCCLfiiiiiii1;G    80GGGGGGGC;1iiiiii1;;;;1i;iiiiiiiiiiiii1i:1G                 8Gf1i;;;i1tfLCf8  
+8GC11CCLiiiiiiii1   0CGGGGGGG01iiiiiiii1;:::;:iiiiiiiiiiiiiiii;i;L             0Cti;;;i1tfLCG08CC   
+   L :fCf;1iiiii;t88GGGGGGGGGt;iiiiiiiii;;;;;;;;;;;;;;;;;;;;;;:;;:1t11L     8Cti;;;i1fLCG000008f8   
+   8,.,LCiiiiiii1iitfLCCCLLtiiiiii;iiiiiiiiiiiii1111tttttttttffffLtiffG   Gti;;;;1fCCGG00000000f    
+    ;..1Ct;1iiiiiiiiiiiiiiiii1ii;iii1fLCCCCCCCCC08888888888888880Li;8   C1;;;:itLCGG0080000008CC    
+    t..;CL;iii1LLfiii1iiiiiii;;iii1LGGCCCLLLLLLLGG000000000088Gti;;:1 Ci:;;:ifCCGG088888888888L0    
+    L.:1CL;iiiifG80Lii1iiii;;iii1fCLLfffLLLLLLLCGGGGGGGGGCGGLt;;i;;;;1:;;:iLGGG0000GGGGGGGGGGGf8    
+  8CtittCL;iiiii;1G801;iiiii1i;tLLLLCCCCCCCCCCG88888888888Gtii:;;;;;;:;;;fCCLCCCCCGGGG0000000CL     
+ 8LLt1tLC1;1iiiiii;f801iiiiiiiLLLLCCCCCCCCCCG000000000080fi;i;:;;;;;;;:;fLfLG0000888888800080f8     
+ CC1ftffi;1iiiiiiii;f80iiiiiii1fLLLLfffffLCG888800000080i;;;;::;;;;;;:1CCC08888000000000088GL0      
+0L8ftt;;::;iiiiiiii1;C8f;1iiiiii1tLCGCCGG0GCCCG00888008f:;;;;:;;;;;;:1GCC0800000000000080GCC8       
+CC8f8 f:;;:::;iiiii1it8G;iiiiii1i::;1L08800800GCCCG0088;;;;;::;;;;;;:fGLG80000000000080GCG8         
+GC8f8  Gi:;;;:::;;;iii001iiiiiii;;;;;,itC888008800GCCGC:;;;;:;;;;;;;;tLG0000000000008GC0            
+0L8LC    Ci::;;:::;;;:G8t:i;;;;;;;;;:;;::;fG8800088800t:;;;:;;;;;;;:iGt0800000000000LG              
+ LG0f8     Gt;:;;;::::C8f:;;;;;;;;;::;;;;,:;1L088000081:;;:;;;:::;;:1GCL08000000008f0               
+ 8f0CL       8C1;:::;:C8L,;:;;;;;::;;;;;::;;::;tC08808;;:::::;1Li;;:1CG8LG80000008CL                
+  0L8LC         8Gf1i:L8L,;:::::::;;::::;;;;;;;;:ifG80:,:itLG8  f:;;iC088LC8000008L0                
+   0f0fG             CL8L11i;:::;::::::::::::::;;:;1CCtG8       0:;;;G8008CL080008f8                
+    0f0fG            8f8LG 8880011t111111111tttffff8 0           1:;:C800080LG8800f                 
+     8f0fG            f0GL     CtLLLLLLLLLLLLLLLLLLtG            G:;:t8000088CLG LG                 
+      8f0fG           CL8f8   CfGCCCLLLLLLLLLLLLLLLLtC            1:i;080000080CLL                  
+       0f8f0          8f0GL  CL80000000GGGGCCCLLLLLLLtf8          0:1;t80000008GL8                  
+        GL0t8          CL8f0Gf8000000000000000000GGGCGLf0          L;1;G800080CC                    
+         LGGf           fG0tL0GCLC00000CG000000000000000LG          1ii18088CL0                     
+         8f8f0          0f8GtCG08f0000LCLC00000000GGG0008CC         0;1;L80LG                       
+          LGCL           Gf8CL   Gf8GL0  GLC00000LCGGCCCCGLf8        G;i;Gf8                        
+          GL0f            Cf8CL8  fLL8     GLG08CL     8800CG         C;i;G                         
+          0L0f             Gf0Gf0 0C        8CLGf8                     Gi;t                         
+          GCCC              0fG0LC            8LG                       8LL                         
+          CLL8               8CL0GLG                                                                
+         0tL8                  0LCGCCG8                                                             
+         LG                      0CCGCCC08                                                          
+                                   8GCCCLLCG08                                                      
+                                      80GCCLfffLL0                                                  
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input('You\'re at a crossroad, where do you want to go? '
+                'Type "left" or "right".\n').lower()
+
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. '
+                    'There is an island in the middle of the lake. '
+                    'Type "wait" to wait for a boat. '
+                    'Type "swim" to swim across.\n').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at the island unharmed. "
+                        "There is house with 3 doors. One red, "
+                        "one yellow and one blue. "
+                        "Which colour do you choose?\n").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over")
+        elif choice3 == "yellow":
+            choice4 =(input("You found the treasure. You Win! Type Y to share or N to own it fully."))
+            if choice4=="Y":
+                print("fuck ya!")
+            else:
+                print("why you like that de.")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You got attacked by an angry trout. Game Over.")
+
+else:
+    print("You fell in to a hole. Game Over.")
